6934cc38eed0e960624434af39e318b97dbf2cb5
Kazim Raza <kazim.friendly@gmail.com>
2018-02-03 15:06:04 +0500

    Email Text Update for responses

--- resources/views/emails/response_by_client.blade.php
+++ resources/views/emails/response_by_client.blade.php
@@ -1,6 +1,6 @@
 <p>Dear {{$recipient}},</p> 
 
-<p>I ({{$user->name}}) have submitted a response to your module <strong>{{$module->title}}</strong>, in your package <strong>{{$package->title}}</strong>. </p>
+<p>{{$user->name}} - submission from  <strong>{{$module->title}}</strong>. <!--, in your package <strong>{{$package->title}}</strong>.--> </p>
 
 <p>To review my comments, <a href="{{$moduleUrl}}">Click this link</a>.</p> 
 
