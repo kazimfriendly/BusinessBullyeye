7b31a17362197a5514ac2c9208b35c6c897f1f57
Kazim Raza <kazim.friendly@gmail.com>
2018-03-14 11:43:13 +0500

    Add Italic Feature in it.

--- resources/views/module/create.blade.php
+++ resources/views/module/create.blade.php
@@ -261,7 +261,7 @@ Modules
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/codemirror.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/codemirror/3.20.0/mode/xml/xml.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/codemirror/2.36.0/formatting.js"></script>
-<script src="{{asset('js/summernote.min.js')}}"></script>
+<script src="{{asset('js/summernote.min.js?v=1')}}"></script>
 <script>
     $(document).ready(function() {
         $('progress').hide();
